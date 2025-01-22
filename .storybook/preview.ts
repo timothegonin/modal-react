87fdd2618a3f61b4d631a5795ac21522cf069ef7
timotheG <gonin.timothe@gmail.com>
2025-01-22 14:51:32 +0100

    🔨 Updating build script

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,7 @@
-/** @type { import('@storybook/react').Preview } */
-const preview = {
+import type { Preview } from '@storybook/react'
+import '../src/index.css'
+
+const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
@@ -8,6 +10,6 @@ const preview = {
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
