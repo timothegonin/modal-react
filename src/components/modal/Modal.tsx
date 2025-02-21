4939aea9b35d307e721f12dc9b1999ae830eb7b5
timotheG <gonin.timothe@gmail.com>
2025-02-21 14:32:24 +0100

    📱 modal behaviors

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -21,9 +21,9 @@ export const Modal = ({
         onClick={onToggle}
         className="ui-fixed ui-flex ui-justify-center ui-items-center ui-bg-slate-500 ui-bg-opacity-70 ui-h-full ui-left-0 ui-top-0 ui-right-0 ui-bottom-0"
       >
-        <div className="ui-flex ui-flex-col ui-bg-white  ui-rounded-md ui-w-1/3 ui-border ui-border-slate-300">
+        <div className="ui-flex ui-flex-col ui-bg-white ui-w-full ui-m-4 ui-rounded-md ui-border ui-border-slate-300 min-[500px]:ui-w-3/4 md:ui-w-1/2">
           {/* modal header */}
-          <div className="ui-flex ui-justify-between ui-p-4 ui-items-center ui-border-b-2">
+          <div className="ui-flex ui-justify-between ui-p-4 ui-items-center ui-border-b-2 ui-gap-4">
             <h5 className="ui-font-semibold ui-text-xl">{title}</h5>
             <button onClick={onToggle} aria-label="Close">
               <svg
